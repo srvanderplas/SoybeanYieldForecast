7d946eca3b795a376af61763655b5455e7e43f6d
Susan VanderPlas (root) <srvanderplas@gmail.com>
2015-04-29 14:41:17 -0500

    Don't add an intercept to standard error temperatures

--- Code/UnitConversions.R
+++ Code/UnitConversions.R
@@ -5,7 +5,7 @@
 
 kgHaToLbAcre <- function(x){ 0.89*x }
 mmToIn <- function(x){  0.0393701*x }
-cToF <- function(x) { 32 + x*9/5 }
+cToF <- function(x, shift=T) { ifelse(shift, 32, 0) + x*9/5 }
 
 convertUnits <- function(df){
   vars <- names(df)
@@ -13,14 +13,19 @@ convertUnits <- function(df){
   mm <- rowSums(sapply(c("SW", "DUL", "LL", "SAT"), function(i) grepl(vars, pattern=i)))>0
   temp <- rowSums(sapply(c("ST"), function(i) grepl(vars, pattern=i)))>0
 
+  is.se <- grepl(vars, pattern="StdError")
+
   for(i in vars[kgha]){
     df[,i] <- kgHaToLbAcre(df[,i])
   }
   for(i in vars[mm]){
     df[,i] <- mmToIn(df[,i])
   }
-  for(i in vars[temp]){
+  for(i in vars[temp&!is.se]){
     df[,i] <- cToF(df[,i])
+  }
+  for(i in vars[temp&is.se]){
+    df[,i] <- cToF(df[,i], shift=F)
   }
   return(df)
 }
